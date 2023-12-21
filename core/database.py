57114f3d9df728fe3194484bf911badce9817415
Arsen Mamatbaev <arsenmamatbaev@mail.ru>
2023-12-21 21:39:35 +0500

    Edit database.py

--- core/database.py
+++ core/database.py
@@ -112,7 +112,7 @@ class DBConnection:
     async def get_admins(self) -> Union[List[User], User]:
         query = "SELECT * FROM admins;"
         admins = await self.__connection.fetch(query)
-        return_admins: list = [User(user_id=)]
+        return_admins = []
         for admin in admins:
             return_admins.append(
                 User(user_id=admin['user_id'],
